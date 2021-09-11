8f17cfd51baacc0eeb9184fb2652fae6b1833d19
Rupin Dhanoa <rdhanoa@cisco.com>
2021-09-11 11:51:08 -0700

    added SMTP email sender

--- chapter15/sendReminders.py
+++ chapter15/sendReminders.py
@@ -25,9 +25,22 @@ for user in range(2, last_row + 1):
             email = sheet.cell(row=user, column=2).value
             unpaid_members[name] = email
 
-
-
-unpaid_members = {}
-
 # TODO: Log in to email account
+email_server = "smtp.gmail.com"
+sender_email = "hellyeah94545@gmail.com"
+sender_password = "ynliulpvyhkwyipd"
+smtpObj = smtplib.SMTP(email_server, 587)
+smtpObj.ehlo()
+smtpObj.starttls()
+smtpObj.login(sender_email, sender_password)
+
 # TODO: Send out reminder emails
+
+for name, email in unpaid_members.items():
+    body = "Subject: {0} dues unpaid. \nDear {1}, \n\nRecords show that you have not paid dues for {0}." \
+           "Please make this payment as soon as possible. \n\nThank you!".format(latest_month, name)
+    send_mail_status = smtpObj.sendmail(sender_email, email, body)
+    print(email)
+    if send_mail_status != {}:
+        print('There was a problem send email to {0}: {1}'.format(email, send_mail_status))
+smtpObj.close()
